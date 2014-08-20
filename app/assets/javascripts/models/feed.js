110a79bda9b4e12e90c89ccf38330b112ab3b924
Evan and Noah <evan+noah@appacademy.io>
2014-08-20 11:17:56 -0400

    Implement nested fetching for entries belonging to feeds.

--- app/assets/javascripts/models/feed.js
+++ app/assets/javascripts/models/feed.js
@@ -1,3 +1,25 @@
 NewsReader.Models.Feed = Backbone.Model.extend({
 
+  entries: function() {
+    if (this._entries) {
+      return this._entries;
+    }
+
+    this._entries = new NewsReader.Collections.Entries([], {
+      feed: this
+    });
+
+    return this._entries;
+  },
+
+  parse: function(response){
+    if (response['latest_entries']){
+      this.entries().set(response['latest_entries'], {parse: true});
+
+      delete response['latest_entries'];
+
+    }
+
+    return response;
+  }
 });
